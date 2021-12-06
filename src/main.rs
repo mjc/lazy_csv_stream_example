3722332479fbe7fe2d19e8c82148c4dc59a550fd
Michael J. Cohen <mjc@kernel.org>
2021-12-06 14:33:54 -0700

    More cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::{
     env,
-    error::Error,
     fs::{File, OpenOptions},
 };
 
 use serde::{Deserialize, Serialize};
 
-use csv::{QuoteStyle, Reader, Trim, Writer, WriterBuilder};
+use csv::{QuoteStyle, Reader, Writer, WriterBuilder, Trim};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MyRow {
@@ -16,37 +15,43 @@ struct MyRow {
 
 // cargo run --release large.csv  53.87s user 3.94s system 85% cpu 1:07.54 total
 
-fn csv_reader_stream(in_file_path: String) -> Result<Reader<File>, Box<dyn Error>> {
+fn csv_reader_stream(in_file_path: String) -> Result<Reader<File>, std::io::Error> {
     let input_file = File::open(in_file_path)?;
     let reader = csv::ReaderBuilder::new()
-        // this is the default
-        .has_headers(true)
-        // also the default
-        .quoting(true)
-        // remove whitespace from beginning and end of headers
-        .trim(Trim::Headers)
-        .from_reader(input_file);
+    .trim(Trim::Headers)
+    .from_reader(input_file);
     Ok(reader)
 }
 
-fn csv_writer_stream(out_file_path: String) -> Result<Writer<File>, Box<dyn Error>> {
+fn csv_writer_stream(out_file_path: String) -> Result<Writer<File>, std::io::Error> {
     // File.open does not create new files by default
     // so we must use OpenOptions manually instead.
-    let output_file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(out_file_path)?;
+    let output_file = open_output_file(out_file_path)?;
     let writer = WriterBuilder::new()
         .quote_style(QuoteStyle::Never)
-        .has_headers(true)
         .from_writer(output_file);
     Ok(writer)
+}
+
+fn open_output_file(out_file_path: String) -> Result<File, std::io::Error> {
+    /*
+        In nightly you can do
+
+        File::with_options()
+            .create(true)
+            .write(true)
+            .open(out_file_path)
+    */
+    OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(out_file_path)
 }
 
 fn stream_read_and_write(
     input_filename: String,
     output_filename: String,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), std::io::Error> {
     let mut reader = csv_reader_stream(input_filename)?;
     let mut writer = csv_writer_stream(output_filename)?;
 
